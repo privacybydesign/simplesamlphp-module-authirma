9ab8380e68831a12468bfcbfcfef68b815c2d71a
Joost van Dijk <vamdijk.joost@gmail.com>
2017-05-25 21:02:52 +0200

    fix path for signing key

--- www/jwt.php
+++ www/jwt.php
@@ -1,27 +1,19 @@
 <?php
 
-// TODO: fix path
-//require_once __DIR__ . '/../vendor/autoload.php';
-require_once '/Users/jodi/irma-idp/simplesamlphp/vendor/autoload.php';
+// TODO: this feels clumsy
+$globalConfig = \SimpleSAML_Configuration::getInstance();
+$vendordir = $globalConfig->resolvePath('vendor');
+require_once "$vendordir/autoload.php";
+
 use \Firebase\JWT\JWT;
 
-if(file_exists('conf.php'))
-    include 'conf.php';
-if (!defined('ROOT_DIR'))       define('ROOT_DIR', __DIR__ . '/../');
-if (!defined('JWT_PRIVATEKEY')) define('JWT_PRIVATEKEY', 'surfnet-idp-sk.pem');
-
 function get_jwt_key() {
-// $pk = openssl_pkey_get_private("file://" . ROOT_DIR . $this->jwt_privatekeyfile);
-//    if( !isset($this->jwt_privatekeyfile) ) // TODO rename signing key?
-//        throw new Exception("JWT signing key missing");
-//    $filename = ROOT_DIR . JWT_PRIVATEKEY;
-    $filename = "/Users/jodi/irma-idp/simplesamlphp/modules/authirma/surfnet-idp-sk.pem";
+    $filename = \SimpleSAML\Utils\Config::getCertPath("irma-idp-sk.pem");
     $pk = openssl_pkey_get_private("file://$filename");
     if ($pk === false)
         throw new Exception("Failed to load signing key");
     return $pk;
 }
-
 
 function get_jwt() {
     $pk = get_jwt_key();
